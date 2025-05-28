b1f921007df97314e9297166010815b5eb5c9884
WebProjectsHub <143710009+WebProjectsHub@users.noreply.github.com>
2025-05-28 15:17:05 +0400

    Update jokerMocking.js

--- Apr_JokersGame/Tools/Chatbox_1/js/jokerMocking.js
+++ Apr_JokersGame/Tools/Chatbox_1/js/jokerMocking.js
@@ -5,11 +5,11 @@ let wrongQuestionCount = 0;
 
 // Mocking messages after multiple wrong questions
 const mockingMessages = [
-    "Are you even trying, detective?",
-    "You're making this too easy for me!",
-    "Come on, a child could solve this faster!",
-    "Maybe detective work isn't your calling!",
-    "At this rate, I'll get bored before you catch me!"
+    { text: "Are you even trying, detective?", audio: "mocking_trying.mp3" },
+    { text: "You're making this too easy for me!", audio:  "mocking_too-easy.mp3" },
+    { text: "Come on, a child could solve this faster!", audio: "mocking_come-on.mp3" },
+    { text: "Maybe detective work isn't your calling!", audio: "mocking_maybe-detective.mp3" },
+    { text: "At this rate, I'll get bored before you catch me!", audio: "mocking_at-this-rate.mp3" },
 ];
 
 // Call this when a wrong question is detected
@@ -19,7 +19,7 @@ function trackWrongQuestion() {
     if (wrongQuestionCount >= 3) {
         wrongQuestionCount = 0; // Reset counter after mocking
         const randomMock = mockingMessages[Math.floor(Math.random() * mockingMessages.length)];
-        simulateJokerTyping(randomMock);
+        simulateJokerTyping(randomMock.text, randomMock.audio);
     }
 }
 
